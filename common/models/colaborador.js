8b4be404954e4a92b3efee8421ffbdb857e9e8f0
Maria Antônia Amado <48838678+AmadoMaria@users.noreply.github.com>
2022-06-19 20:48:45 -0300

    Ajustando aggregates de get por data

--- common/models/colaborador.js
+++ common/models/colaborador.js
@@ -1,52 +1,38 @@
 'use strict';
+const { ObjectID } = require('mongodb');
 
 module.exports = function(Colaborador) {
     Colaborador.getMedidasData = async function (data, id) {
         var dataAux = new Date(data);
         var dataAux2 = new Date(data);
         dataAux2.setDate(dataAux.getDate() + 1);
-    
+        
         const collection = Colaborador.getConnector().collection('Colaborador');
-        const cursor = collection.aggregate({
-          where: { _id: id },
-          aggregate: [
-            {
-              $unwind: {
-                path: "$historicoMedidas",
-              },
-            },
-            {
-              $match: {
-                $and: [
-                  {
-                    "historicoMedidas.dataHora": {
-                      $gte: dataAux,
-                    },
-                  },
-                  {
-                    "historicoMedidas.dataHora": {
-                      $lt: dataAux2,
-                    },
-                  },
-                ],
-              },
-            },
-            {
-              $project: {
-                _id: 0,
-                peso: "$historicoMedidas.peso",
-                altura: "$historicoMedidas.altura",
-                dataHora: "$historicoMedidas.dataHora",
-                imc: "$historicoMedidas.imc",
-              },
-            },
-            {
-              $sort:{
-                dataHora: -1
-              }
+      const cursor = collection.aggregate([
+        {
+          '$match': {
+            '_id': new ObjectID(id)
+          }
+        },
+          {
+            '$unwind': {
+              'path': '$historicoMedidas'
             }
-          ],
-        })
+          }, {
+            '$project': {
+              '_id': 0, 
+              'peso': '$historicoMedidas.peso', 
+              'altura': '$historicoMedidas.altura', 
+              'dataHora': '$historicoMedidas.dataHora', 
+              'imc': '$historicoMedidas.imc'
+            }
+          }, {
+            '$sort': {
+              'dataHora': -1
+            }
+          }
+        ]
+        )
 
         return cursor.toArray();
       };
@@ -188,8 +174,7 @@ module.exports = function(Colaborador) {
 
     Colaborador.getResumoImc = async function () {
         const collection = Colaborador.getConnector().collection('Colaborador');
-        const cursor = collection.aggregate({
-            aggregate: [
+        const cursor = collection.aggregate([
             { $sortByCount: "$medida.imc.classificacao" },
             {
                 $project: {
@@ -198,8 +183,7 @@ module.exports = function(Colaborador) {
                 y: "$count",
                 },
             },
-            ],
-        });
+            ]);
 
             return cursor.toArray();
     };
@@ -218,8 +202,7 @@ module.exports = function(Colaborador) {
 
     Colaborador.getNumCadastros = async function () {
         const collection = Colaborador.getConnector().collection('Colaborador');
-        const cursor = collection.aggregate({
-            aggregate: [
+        const cursor = collection.aggregate([
             {
                 $project: {
                 dataCadastro: {
@@ -312,8 +295,7 @@ module.exports = function(Colaborador) {
                 ],
                 },
             },
-            ],
-        })
+            ])
             return cursor.toArray();
     };
 
@@ -328,5 +310,138 @@ module.exports = function(Colaborador) {
             root: true,
         },
     });
+
+    Colaborador.getUltimaAvaliacaoColaborador = async function (id) {
+        const collection = Colaborador.getConnector().collection('Colaborador');
+        const cursor = collection.aggregate([
+            {
+              $match: {
+                _id: new ObjectID(id)
+              }
+            },
+                {
+                    $lookup: {
+                        from: "Avaliacao",
+                        localField: "_id",
+                        foreignField: "ColaboradorId",
+                        as: "avaliacoes",
+                    },
+                },
+                {
+                    $unwind: {
+                        path: "$avaliacoes",
+                    },
+                },
+                {
+                    $group: {
+                        _id: "$_id",
+                        dataHora: {
+                            $max: "$avaliacoes.dataHora",
+                        },
+                        pulso: {
+                            $last: "$avaliacoes.pulso",
+                        },
+                        pSistolica: {
+                            $last: "$avaliacoes.pSistolica",
+                        },
+                        pDiastolica: {
+                            $last: "$avaliacoes.pDiastolica",
+                        },
+                        hipertensao: {
+                            $last: "$avaliacoes.hipertensao",
+                        },
+                    },
+    
+                },
+                {
+                    $sort: {
+                        dataHora: -1
+                    }
+                }
+            ]);
+        return cursor.toArray();
+      };
+    
+      Colaborador.remoteMethod("getUltimaAvaliacaoColaborador", {
+        description: "Retorna a última avaliação de um colaborador",
+        accepts: [
+          {
+            arg: "id",
+            type: "string",
+            required: true,
+          },
+        ],
+        http: {
+          path: "/:id/avaliacao/ultima",
+          verb: "get",
+        },
+        returns: {
+          type: {},
+          root: true,
+        },
+      });
+    
+  Colaborador.getAvaliacaoColaboradorData = async function (id, data) {
+        var dataAux = new Date(data);
+        var dataAux2 = new Date(data);
+        dataAux2.setDate(dataAux.getDate() + 1);
+    
+        const collection = Colaborador.getConnector().collection('Colaborador');
+        const cursor = collection.aggregate([
+            {
+              '$match': {
+                '_id': new ObjectID(id)
+              }
+            }, {
+              '$lookup': {
+                'from': 'Avaliacao', 
+                'localField': '_id', 
+                'foreignField': 'colaborador', 
+                'as': 'avaliacoes'
+              }
+            }, {
+              '$unwind': {
+                'path': '$avaliacoes'
+              }
+            }, {
+              '$project': {
+                '_id': 0, 
+                'id': '$avaliacoes._id', 
+                'hipertensao': '$avaliacoes.hipertensao', 
+                'pSistolica': '$avaliacoes.pSistolica', 
+                'pDiastolica': '$avaliacoes.pDiastolica', 
+                'pulso': '$avaliacoes.pulso', 
+                'dataHora': '$avaliacoes.dataHora'
+              }
+            }
+          ]);
+
+        return cursor.toArray();
+      };
+    
+      Colaborador.remoteMethod("getAvaliacaoColaboradorData", {
+        description:
+          "Retorna as avaliações de um colaborador em uma data específica",
+        accepts: [
+          {
+            arg: "id",
+            type: "string",
+            required: true,
+          },
+          {
+            arg: "data",
+            type: "string",
+            required: true,
+          },
+        ],
+        http: {
+          path: "/:id/avaliacao/:data",
+          verb: "get",
+        },
+        returns: {
+          type: {},
+          root: true,
+        },
+      });
 
 };
